de07c50665f600a74f6c00a61df4d7b4a7f3e99d
Babasola Oso <easybabasola@gmail.com>
2021-12-01 05:36:33 +0100

    Uploading Files With Node.js

---
+++ fileUpload/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const upload = require('express-fileupload')
+
+const app = express()
+
+app.use(upload())
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/', (req, res) => {
+    if(req.files){
+        console.log(req.files)
+
+        var file = req.files.file
+        var filename = file.name
+        console.log("The Name Of The File Is " + filename)
+
+        file.mv('./uploads/'+filename, function(err){
+            if(err){
+                res.send(err)
+            }
+            else{
+                res.send("File Uploaded")
+            }
+        })
+    }
+})
+
+app.listen(5000)
